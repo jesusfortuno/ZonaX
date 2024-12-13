9012867706f5e1a089a1d4a621fa87d2d46809b8
victormartin <victormartin@ceroca.cat>
2024-12-13 15:58:00 +0100

    Cambio de index

--- index.php
+++ index.php
@@ -1,11 +1,48 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+// index.php
+session_start();
+
+$action = $_GET['action'] ?? null;
+
+switch ($action) {
+
+    // Resources
+    case 'llistar-categories':
+        include __DIR__.'/resource_llistar_categories.php';
+        break;
+
+    case 'registre':
+        include __DIR__.'/resource_registre.php';
+        break;
+
+    case 'llistar-productes':
+        include __DIR__.'/resource_productos.php';
+        break;
+
+    case 'inicio-sesion':
+        include __DIR__.'/resource_iniciar.php';
+        break;
+
+    case 'registre-session':
+        include __DIR__.'/controller/almacenar_registro.php';
+        break;
+
+    case 'inicio-session':
+        include __DIR__.'/controller/iniciar_sesion.php';
+        break;
+
+    case 'ejemplo':
+        include __DIR__.'/resource_ejemplo.php';
+        break;
+
+    case 'boton':
+        include __DIR__.'/resource_ajax.php';
+        break;
+
+    // Default
+    default:
+        include __DIR__.'/resource_portada.php';
+        break;
+}
+
 ?>
-Something is wrong with the XAMPP installation :-(
