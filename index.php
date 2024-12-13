9d62ad2df0dd33f546ff3e6216e0c0d6d4b57cb8
Jesus Fortuño Blanco <jesus_fortuno@ceroca.cat>
2024-12-13 16:48:34 +0100

    index cambiado

--- index.php
+++ index.php
@@ -19,24 +19,12 @@ switch ($action) {
         include __DIR__.'/resource_productos.php';
         break;
 
-    case 'inicio-sesion':
-        include __DIR__.'/resource_iniciar.php';
-        break;
-
     case 'registre-session':
         include __DIR__.'/controller/almacenar_registro.php';
         break;
 
     case 'inicio-session':
-        include __DIR__.'/controller/iniciar_sesion.php';
-        break;
-
-    case 'ejemplo':
-        include __DIR__.'/resource_ejemplo.php';
-        break;
-
-    case 'boton':
-        include __DIR__.'/resource_ajax.php';
+        include __DIR__.'/controller/llistar_iniciar_sesion.php';
         break;
 
     // Default
